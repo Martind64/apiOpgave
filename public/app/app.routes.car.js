2bb9300740612d3fe180ba89732d1e39d2638159
Martind64 <martind_64@hotmail.com>
2016-03-03 11:35:05 +0100

    added carService & CTRL to index file

--- public/app/app.routes.car.js
+++ public/app/app.routes.car.js
@@ -1,33 +1,33 @@
 angular.module('app.routes.car', ['ngRoute'])
 
-	.config(function($routeProvider $locationProvider) {
+.config(function($routeProvider $locationProvider) {
 
 
-		$routeProvider
+	$routeProvider
 
-		.when('/cars',
-		{
-			templateUrl : 'app/components/cars/all.html',
-			controller  : 'carController',
-			controllerAs: 'car'
-		})
+	.when('/cars',
+	{
+		templateUrl : 'app/components/cars/views/all.html',
+		controller  : 'carController',
+		controllerAs: 'car'
+	})
 
-		// Route to create a cars
-		.when('/cars/create',
-		{
-			templateUrl : 'app/components/cars/single.html',
-			controller  : 'carCreateController',
-			controllerAs: 'car'
-		})
+	// Route to create a cars
+	.when('/cars/create',
+	{
+		templateUrl : 'app/components/cars/views/single.html',
+		controller  : 'carCreateController',
+		controllerAs: 'car'
+	})
 
-		// Route to update a cars
-		.when('/cars/:restaurant_id',
-		{
-			templateUrl : 'app/components/cars/single.html',
-			controller  : 'carEditController',
-			controllerAs: 'car'
-		});
-
-		$locationProvider.html5Mode(true);
-
+	// Route to update a cars
+	.when('/cars/:restaurant_id',
+	{
+		templateUrl : 'app/components/cars/views/single.html',
+		controller  : 'carEditController',
+		controllerAs: 'car'
 	});
+
+	$locationProvider.html5Mode(true);
+
+});
